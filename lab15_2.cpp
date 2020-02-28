d38f9d449627c7e2d5c306a70aa0e2bad9e1269e
shoukugeki02 <59277996+shoukugeki02@users.noreply.github.com>
2020-02-28 17:27:53 +0700

    Update lab15_2.cpp

--- lab15_2.cpp
+++ lab15_2.cpp
@@ -5,23 +5,53 @@ using namespace std;
 
 void randData(double *,int,int);
 
-void findColSum(const double *,double *,int,int);
 
-void showData(double *,int,int);
+
+void findColSum(const double*,double *,int,int);
+
+	
+
+void showData(double *,int ,int );
 
 int main(){
 	srand(time(0));
 	const int N = 7, M = 7;
 	double data[N][M] = {};
-	double result[M] = {};
+	double result[N] = {};
 	double *dPtr = data[0];
 	randData(dPtr,N,M);
 	showData(dPtr,N,M);
 	
 	cout << "---------------------------------------------\n";
 	
-	findColSum(dPtr,result,N,M); 
-	showData(result,1,M);
+	findColSum(dPtr,result,N,M);
+	showData(result,1,N);
 }
 
-//Write definition of randData(), showData() and findColSum()
+//Write definition of randData(), showData() and findRowSum()
+void randData(double *dPtr,int N,int M){
+	for(int i=0;i<N;i++){
+		for(int j=0;j<M;j++){
+			float x =rand()%100;
+			*(dPtr+(M*i)+j) = x/100;
+		}
+	}
+}
+
+void findColSum(const double *dPtr,double *result,int N,int M){
+	for(int i=0;i<N;i++){
+		for(int j=0;j<M;j++){
+			*(result+j) += *(dPtr+(M*i)+j);
+		}
+	}
+
+}
+
+void showData(double *dPtr,int N,int M){
+	for(int i=0;i<N;i++){
+		for(int j=0;j<M;j++){
+			cout << *(dPtr+(M*i)+j) << " ";
+		}
+		cout << "\n";
+	}
+}
